1abe880aab1f136f12461e3769ead18820de5d88
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-03-09 12:12:04 -0800

    adding all changes made when working with help; we created 2 different local storage methods to get prior stored cities upon opening the website, and getting histroy plus current city from local storage to update history when user searches a new city; added a variable to take data received from city search and print it to the website.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,9 @@
 let input = $("#input");
 let search = $(".btn");
 let history = $("#history");
-
+let pastCities = document.createElement("ul");
+pastCities.setAttribute("class","past-cities list-unstyled");
+//let pastCity = document.createElement("li");
 //Present weather tabs declared in variables
 let present = $("#city");
 let cityName = $("#cityName");
@@ -35,7 +37,7 @@ let day2Hum = $("#day2Hum");
 let day3Hum = $("#day3Hum");
 let day4Hum = $("#day4Hum");
 let day5Hum = $("#day5Hum");
-let cities = [];
+//let cities = [];
     
 // cities.push(localStorage.getItem("cities", cities));
 // console.log(cities);
@@ -43,36 +45,81 @@ let cities = [];
 search.click(function() {
     let newCity = input.val();
     
-    cities.push(newCity);
-    console.log(cities);
-    localStorage.setItem("cities", JSON.stringify(cities));
-    history.createElement("ul");
+    //cities.push(newCity);
+    //console.log(cities);
+
+    const history = JSON.parse(localStorage.getItem("cities"));
+
+    history.push(newCity);
+
+    localStorage.setItem("cities", JSON.stringify(history));
+
+    let listItems = "";
+    for (let i = 0; i < history.length; i++) {
+        listItems += "<li class='list-city-item'>"+history[i]+"</li>";
+    };
+
+    $(".past-cities").html(listItems);
+
+    searchCity();
 })
+
+    let pastCitiesHistory = JSON.parse(localStorage.getItem("cities"));
+
+    console.log("*****",pastCitiesHistory);
+    history.append(pastCities); // ul 
+
+    let listItems = "";
+    for (let i = 0; i < pastCitiesHistory.length; i++) {
+        listItems += "<li class='list-city-item'>"+pastCitiesHistory[i]+"</li>";
+    };
+
+    $(".past-cities").html(listItems);
+
+
 //fetches information needed for present city weather
 function searchCity() {
-    let requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + input.val() + "&appid=217bed3cfe116291c85bc4819a64b5e0"
+    let requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + input.val() + "&appid=217bed3cfe116291c85bc4819a64b5e0&units=imperial"
 
-    fetch (requestURL, {
-        units: "imperial"
-    })
+    fetch (requestURL)
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
         console.log(data);
-        present.addClass("col-12 col-md-8");
-        for (var i = 0; i < data.length; i++) {
-            cityName.texContent = data[i].name;
-            currentDay.textcontent = moment().subtract(10, "days").calendar();
-            cityIcon.textContent = data[i].weather.icon;
-            temp.textContent = data[i].main.temp;
-            humidity.textContent = data[i].main.humidity;
-            windSpeed.textContent = data[i].wind.speed;
-        }
+        present.removeClass("d-none");
+        // for (var i = 0; i < data.length; i++) {
+        //     cityName.texContent = data[i].name;
+        //     currentDay.textcontent = moment().subtract(10, "days").calendar();
+        //     cityIcon.textContent = data[i].weather.icon;
+        //     temp.textContent = data[i].main.temp;
+        //     humidity.textContent = data[i].main.humidity;
+        //     windSpeed.textContent = data[i].wind.speed;
+        // }
+
+        // </span>
+        //         <span id="currentDay"><span id="cityIcon"></span></span>
+
+        const currentDayMarkUp = 
+        `
+            <h2 id="cityName">
+                <span>${data.name}</span>
+                <span>${Date.now}</span>
+                <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png"/>
+                
+            </h2>
+            <div id="temp">${data.main.temp} F</div>
+            <div id="humidity">${data.main.humidity} %</div>
+            <div id="windSpeed">${data.wind.speed}</div>
+            <div id="uv"></div>
+        `;
+
+        $("#cityInfo").html();
+
     }).catch(function(err) {
         console.log(err);
     })
 }
 
 // //Adds event listener for when the user searches a city
-search.on("click", searchCity(input.value));
+//search.on("click", searchCity);
