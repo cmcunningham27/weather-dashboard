83430dc35cafff30fd0f434d7c5b37b7051503e7
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-03-09 23:37:31 -0800

    fixed some errors that were happening due to complete refresh of local storage by applying if statements to check if the local storage is empty; I then changed font sizes and realized that the CSS file was not linked to the HTML so added that external link; begun a fetch request for the future 5 day forcast for the current city, and still need to finish it; need to figure out how to render the first city searched onto the website.

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ let lon;
 let uv = $("#uv");
 
 //Future weather tabs declared in variables
-let futureDivs = $(".future");
+let futureDivs = $("#5Day");
 let day1 = $("#day1");
 let day2 = $("#day2");
 let day3 = $("#day3");
@@ -48,39 +48,61 @@ search.click(function() {
 
     let newCity = input.val();
 
-    const history = JSON.parse(localStorage.getItem("cities"));
-
-    if ($.inArray(JSON.parse(JSON.stringify(newCity)), history) >= 0) {
-        console.log("Search a new city");
-        return;
-        
-    } else {
-        history.push(newCity);
-
-        localStorage.setItem("cities", JSON.stringify(history));
-
+    if (localStorage.getItem("cities") === null) {
+        const newInput = [];
+        newInput.push(newCity);
+        localStorage.setItem("cities", JSON.stringify(newInput));
+        const history = JSON.parse(localStorage.getItem("cities"));
+        console.log(history);
         let listItems = "";
         for (let i = 0; i < history.length; i++) {
-            listItems += "<li class='list-city-item'>"+history[i]+"</li>";
+            listItems += "<li class='list-city-item border'>"+history[i]+"</li>";
         };
 
         $(".past-cities").html(listItems);
 
         searchCity(newCity);
+    } else {
+
+    history = JSON.parse(localStorage.getItem("cities"));
+
+    
+    if ($.inArray(JSON.parse(JSON.stringify(newCity)), history) >= 0) {
+        console.log("Search a new city");
+        return;
     }
-})
+    
+    history.push(newCity);
 
-    let pastCitiesHistory = JSON.parse(localStorage.getItem("cities"));
+    localStorage.setItem("cities", JSON.stringify(history));
 
-    console.log("*****",pastCitiesHistory);
-    history.append(pastCities); // ul 
-
-    let listItems = "";
-    for (let i = 0; i < pastCitiesHistory.length; i++) {
-        listItems += "<li class='list-city-item'>"+pastCitiesHistory[i]+"</li>";
+    listItems = "";
+    for (let i = 0; i < history.length; i++) {
+        listItems += "<li class='list-city-item border p-3'>"+history[i]+"</li>";
     };
 
     $(".past-cities").html(listItems);
+
+    searchCity(newCity);
+    }
+})
+    if (localStorage.getItem("cities") === null) {
+        console.log("Local Storage Empty");
+    } else {
+
+        let pastCitiesHistory = JSON.parse(localStorage.getItem("cities"));
+
+        console.log("*****",pastCitiesHistory);
+        history.append(pastCities); // ul 
+
+        let listItems = "";
+        for (let i = 0; i < pastCitiesHistory.length; i++) {
+            listItems += "<li class='list-city-item border p-3'>"+pastCitiesHistory[i]+"</li>";
+        };
+
+        $(".past-cities").html(listItems);
+    
+    }
 
 
 //fetches information needed for present city weather
@@ -98,21 +120,22 @@ function searchCity() {
         
         const currentDayMarkUp = 
         `
-            <h2 id="cityName">
+            <h4 id="cityName">
                 <span>${data.name}</span>
-                <span>${moment().subtract(10, "days").calendar()}</span>
+                <span>(${moment().format("MM/DD/YYYY")})</span>
                 <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png"/>
         
-            </h2>
-            <div id="temp">Temperature: ${data.main.temp} &#xb0;F</div>
-            <div id="humidity">Humidity: ${data.main.humidity} %</div>
-            <div id="windSpeed">Wind Speed: ${data.wind.speed} MPH</div>
-            <div id="uv">UV Index: </div>
+            </h4>
+            <div id="temp" class="p-2">Temperature: ${data.main.temp} &#xb0;F</div>
+            <div id="humidity" class="p-2">Humidity: ${data.main.humidity} %</div>
+            <div id="windSpeed" class="p-2">Wind Speed: ${data.wind.speed} MPH</div>
+            <div id="uv" class="p-2">UV Index: </div>
         `;
 
         $("#cityInfo").html(currentDayMarkUp);
         let lat = data.coord.lat;
         let lon = data.coord.lon;
+
 
     let requestUV = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=217bed3cfe116291c85bc4819a64b5e0&units=imperial"
 
@@ -126,8 +149,56 @@ function searchCity() {
         const currentUV =
         `
             <div id="uv">UV Index: ${data.value}</div>
-        `
+        `;
+
         $("#uv").html(currentUV);
+
+
+    let requestFive = "https://api.openweathermap.org/data/2.5/forecast?q=" + input.val() + "&cnt=5&appid=217bed3cfe116291c85bc4819a64b5e0&units=imperial"
+
+    fetch(requestFive)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        futureDivs.removeClass("d-none");
+
+        const currentFive =
+        `
+        <div class="future col-2.5 bg-primary rounded mx-1 text-light">
+        <h5 id="day1" class="fw-bold">${moment().add(1, "days").format("MM/DD/YYYY")}</h5>
+        <div id="day1Icon">${data.list[0].weather[0].icon}.png</div>
+        <div id="day1Temp">Temp: ${data.list[0].main.temp}&#xb0;</div>
+        <div id="day1Hum">${data.list[0].main.humidity}</div>
+    </div>
+    <div class="future col-2 bg-primary rounded mx-1 text-light">
+        <h5 id="day2">${moment().add(2, "days").format("MM/DD/YYYY")}</h5>
+        <div id="day2Icon">${data.list[0].weather[0].icon}.png</div>
+        <h6 id="day2Temp"></h6>
+        <h6 id="day2Hum"></h6>
+    </div>
+    <div class="future col-2.5 bg-primary rounded mx-1 text-light">
+        <h5 id="day3">${moment().add(3, "days").calendar()}</h5>
+        <div id="day3Icon"></div>
+        <h6 id="day3Temp"></h6>
+        <h6 id="day3Hum"></h6>
+    </div>
+    <div class="future col-2 bg-primary rounded mx-1 text-light">
+        <h5 id="day4">${moment().add(4, "days").calendar()}</h5>
+        <div id="day4Icon"></div>
+        <h6 id="day4Temp"></h6>
+        <h6 id="day4Hum"></h6>
+    </div>
+    <div class="future col-2 bg-primary rounded mx-1 text-light">
+        <h5 id="day5">${moment().add(5, "days").calendar()}</h5>
+        <div id="day5Icon"></div>
+        <h6 id="day5Temp"></h6>
+        <h6 id="day5Hum"></h6>
+    </div>
+        `
+        $("#futureWeather").html(currentFive);
+    })
     
         // let requestFive = "https://api.openweathermap.org/data/forecast?"
     }).catch(function(err) {
