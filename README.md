0990141ed8cb52215def0eb301b25500f12e64d8
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-03-16 09:34:33 -0700

    created README.md and any last changes necessary.

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # weather-dashboard
+
+---
+
+A weather application that allows users to check current and future weather for city of their choice, or look at weather for a city they searched in the past. You will find that the app will run in the browser and features dynamic HTML and CSS utilizing Bootstrap, Javascript, jQuery, and Moment.js.
+
+&nbsp;
+
+## Links
+
+---
+
+[Deployed Application URL]( https://cmcunningham27.github.io/weather-dashboard/)
+
+[GitHub Repository URL](https://github.com/cmcunningham27/weather-dashboard)
+
+&nbsp;
+
+## The following image shows how the webpage appears on a desktop
+
+---
+
+[website screenshot](./assets/images/website.png)
+
+&nbsp;
+
+## About the Project
+
+---
+
+I needed to create a weather dashboard that allowed users to see the current and next 5 days' weather for a specific city. To do this I needed to have an input field where the user could type a city name. Once they click the search button 3 fetch requests, from a weather API, gather needed information to render onto the website. The first request gathers the current weather (in imperial units): current date (using Moment.js), weather icon, temp, humidity, and wind speed. The second request gathers the current weather's UV index by inputing the lon and lat from the first request, and changes the background color of the number collected according to whether the conditions are favorable (green), moderate (yellow), or severe (red). The third request gathers the future 5 day forecasts': date (using Moment.js), weather icon for each day, temp, and humidity
+
+&nbsp;
+
+Each time the user searches a city, that city is stored in their local storage, and collected to create a history list. I then gave each of the history cities the on() method to ensure that when the user clicks on a listed city, they will be provided that city's current and future 5 day forecast. If the user types in a city they had already searched previously, they will be told to search a new city name. 
+
+&nbsp;
+
+## Complications I Came Across
+
+---
+
+I ran into a problem when I wanted to change HTML elements after a city had been searched. I received help that showed me a more simple approach by typing in actual HTML tags and their content, and then appending it to the parent element. This I had never seen before, but came in very handy when I needed to replace original elements with new, updated elements.
+
+&nbsp;
+
+When I deleted my local storage I had found that after all my changes and updates I no longer had the initial city stored in local storage. It took some time and trail-and-error on my part, but I finally figured out how to get it stored when the local storage was empty. After that I had to figure out how to have it rendered onto the website in it's parent element, instead of waiting till the user searches a second city. 
+
+&nbsp;
+
+Another issue I came across was figuring out how to change the background color of just the UV index number. When I received help, it became known that I had actually been on the right track, just forgot a closing quotation mark. After everything was finished and just about ready to be turned in the day it was due, I wound up with an error I haven't seen yet. 
+
+&nbsp;
+
+## How the User Can Use the Website
+
+---
+
+When the user opens the page they will see a search box and button. The text tells them to search for a city. They will type in a city name and click the search button with their mouse. Once they do this they will be shown the name of the city they searched, and the current and future 5-day forecast for their searched city. If they wish to search another city, they can go back to the search box and type a new city name. When they have searched a few cities and created a list of history, they can click on a city they have already searched by clicking on its name below the search box.
+
+
+&nbsp;
+
+
+## Credits
+
+---
+
+Cassandra Cunningham, https://github.com/cmcunningham27
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,25 @@
-//Search tabs declared in variables
+//Search elements declared and given values in variables
 let input = $("#input");
 let search = $(".btn");
 let historyCities = $("#historyCities");
 let pastCities = document.createElement("ul");
 pastCities.setAttribute("class","past-cities list-unstyled");
 let listCityItem = $(".list-city-item");
-//Present weather tabs declared in variables
-let present = $("#city");
+
+//Present weather tags declared and given values in variables
+let present = $("#cityInfo");
 
 
-// //Future weather tabs declared in variables
+// //Future weather tags declared and given values in variables
 let futureDivs = $("#5Day");
 
 
-
+//runs code when user types in a city and clicks the search button with their mouse
 search.click(function() {
 
     let newCity = input.val();
 
+    //If they have not searched a city yet the code will add their current city to local storage and then render it onto the webpage
     if (localStorage.getItem("cities") === null) {
         const newInput = [];
         newInput.push(newCity);
@@ -31,8 +33,11 @@ search.click(function() {
         };
 
         $(".past-cities").html(listItems);
-
+        
+        //calss the searchCity function
         searchCity(newCity);
+
+    //if they have history of searched cities in their local storage code runs to add the new search and old to the website
     } else {
 
         history = JSON.parse(localStorage.getItem("cities"));
@@ -54,9 +59,12 @@ search.click(function() {
 
         $(".past-cities").html(listItems);
 
+        //calls the searchCity function
         searchCity(newCity);
     }
 })
+
+    //renders past history onto page as user opens or refreshes the page
     if (localStorage.getItem("cities") === null) {
         console.log("Local Storage Empty");
     } else {
@@ -88,7 +96,7 @@ function searchCity(word) {
         console.log(data);
         present.removeClass("d-none");
         
-        
+        //renders html elements and content onto website in place of original html elements and inserting path to information
         const currentDayMarkUp = 
         `
             <h4 id="cityName">
@@ -104,10 +112,12 @@ function searchCity(word) {
         `;
 
         $("#cityInfo").html(currentDayMarkUp);
+
+        //collects latitude and longitude data information for next fetch request
         let lat = data.coord.lat;
         let lon = data.coord.lon;
 
-
+    //fetches information needed for the current city's uv index number
     let requestUV = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=217bed3cfe116291c85bc4819a64b5e0&units=imperial"
 
     fetch (requestUV)
@@ -117,21 +127,24 @@ function searchCity(word) {
     .then(function(data) {
         console.log(data);
         
+        //renders html element by replacing old uv div and inserting path to information
         const currentUV =
         `
-            <div id="uv" class="p-2">UV Index: ${data.value}</div>
+            <div id="uv" class="p-2">UV Index: <span id="number">${data.value}</span></div>
         `;
 
         $("#uv").replaceWith(currentUV);
+        
+        //Changes background color of uv index number according to how hazardous it is
+        if (data.value < 3) {
+            $("#number").css({"background-color":"#00FF00","border-radius":"5px","padding":"4px"});
+        } else if (data.value >= 3 && data.value < 6) {
+            $("#number").css({"background-color":"#FFFF00","border-radius":"5px","padding":"4px"});
+        } else {
+            $("#number").css({"background-color":"#FF0000","border-radius":"5px","padding":"4px"});
+        }
 
-        // if (data.value < 3) {
-        //     document.getElementById("#uvColor").style.backgroundColor = "#00FF00";
-        // } else if (data.value >= 3 && data.value < 6) {
-        //     document.getElementById("#uvColor").style.backgroundColor = "#";
-        // } else {
-        //     document.getElementById("#uvColor").style.backgroundColor = "#FF0000";
-        // }
-
+    //fetches information needed for the future 5 day forecast of searched city
     let requestFive = "https://api.openweathermap.org/data/2.5/forecast?q=" + word + "&cnt=5&appid=217bed3cfe116291c85bc4819a64b5e0&units=imperial"
 
     fetch(requestFive)
@@ -142,43 +155,45 @@ function searchCity(word) {
         console.log(data);
         futureDivs.removeClass("d-none");
 
+        //renders html elements by replacing original html elements and inserting path to information
         const currentFive =
         `
-        <div class="future col-2 bg-primary rounded mx-2 py-2 text-light">
-        <h5 id="day1" class="fw-bold">${moment().add(1, "days").format("MM/DD/YYYY")}</h5>
-        <div id="day1Icon"><img src="http://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png"/></div>
-        <div id="day1Temp">Temp: ${data.list[0].main.temp}&#xb0;F</div>
-        <div id="day1Hum">Humidity: ${data.list[0].main.humidity}%</div>
-    </div>
-    <div class="future col-2 bg-primary rounded mx-2 py-2 text-light">
-        <h5 id="day2">${moment().add(2, "days").format("MM/DD/YYYY")}</h5>
-        <div id="day2Icon"><img src="http://openweathermap.org/img/w/${data.list[1].weather[0].icon}.png"/></div>
-        <div id="day2Temp">Temp: ${data.list[1].main.temp}&#xb0;F</div>
-        <div id="day2Hum">Humidity: ${data.list[1].main.humidity}%</div>
-    </div>
-    <div class="future col-2 bg-primary rounded mx-2 py-2 text-light">
-        <h5 id="day3">${moment().add(3, "days").format("MM/DD/YYYY")}</h5>
-        <div id="day3Icon"><img src="http://openweathermap.org/img/w/${data.list[2].weather[0].icon}.png"/></div>
-        <div id="day3Temp">Temp: ${data.list[2].main.temp}&#xb0;F</div>
-        <div id="day3Hum">Humidity: ${data.list[2].main.humidity}%</div>
-    </div>
-    <div class="future col-2 bg-primary rounded mx-2 py-2 text-light">
-        <h5 id="day4">${moment().add(4, "days").format("MM/DD/YYYY")}</h5>
-        <div id="day4Icon"><img src="http://openweathermap.org/img/w/${data.list[3].weather[0].icon}.png"/></div>
-        <div id="day4Temp">Temp: ${data.list[3].main.temp}&#xb0;F</div>
-        <div id="day4Hum">Humidity: ${data.list[3].main.humidity}%</div>
-    </div>
-    <div class="future col-2 bg-primary rounded mx-2 py-2 text-light">
-        <h5 id="day5">${moment().add(5, "days").format("MM/DD/YYYY")}</h5>
-        <div id="day5Icon"><img src="http://openweathermap.org/img/w/${data.list[4].weather[0].icon}.png"/></div>
-        <div id="day5Temp">Temp: ${data.list[4].main.temp}&#xb0;F</div>
-        <div id="day5Hum">Humidity: ${data.list[4].main.humidity}%</div>
-    </div>
+        <div class="future col-12 col-sm-5 col-md-3 col-lg-2 bg-primary rounded m-2 py-2 text-light">
+            <h5 id="day1" class="fw-bold">${moment().add(1, "days").format("MM/DD/YYYY")}</h5>
+            <div id="day1Icon"><img src="http://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png"/></div>
+            <div id="day1Temp">Temp: ${data.list[0].main.temp}&#xb0;F</div>
+            <div id="day1Hum">Humidity: ${data.list[0].main.humidity}%</div>
+        </div>
+        <div class="future col-12 col-sm-5 col-md-3 col-lg-2 bg-primary rounded m-2 py-2 text-light">
+            <h5 id="day2">${moment().add(2, "days").format("MM/DD/YYYY")}</h5>
+            <div id="day2Icon"><img src="http://openweathermap.org/img/w/${data.list[1].weather[0].icon}.png"/></div>
+            <div id="day2Temp">Temp: ${data.list[1].main.temp}&#xb0;F</div>
+            <div id="day2Hum">Humidity: ${data.list[1].main.humidity}%</div>
+        </div>
+        <div class="future col-12 col-sm-5 col-md-3 col-lg-2 bg-primary rounded m-2 py-2 text-light">
+            <h5 id="day3">${moment().add(3, "days").format("MM/DD/YYYY")}</h5>
+            <div id="day3Icon"><img src="http://openweathermap.org/img/w/${data.list[2].weather[0].icon}.png"/></div>
+            <div id="day3Temp">Temp: ${data.list[2].main.temp}&#xb0;F</div>
+            <div id="day3Hum">Humidity: ${data.list[2].main.humidity}%</div>
+        </div>
+        <div class="future col-12 col-sm-5 col-md-3 col-lg-2 bg-primary rounded m-2 py-2 text-light">
+            <h5 id="day4">${moment().add(4, "days").format("MM/DD/YYYY")}</h5>
+            <div id="day4Icon"><img src="http://openweathermap.org/img/w/${data.list[3].weather[0].icon}.png"/></div>
+            <div id="day4Temp">Temp: ${data.list[3].main.temp}&#xb0;F</div>
+            <div id="day4Hum">Humidity: ${data.list[3].main.humidity}%</div>
+        </div>
+        <div class="future col-12 col-sm-5 col-md-3 col-lg-2 bg-primary rounded m-2 py-2 text-light">
+            <h5 id="day5">${moment().add(5, "days").format("MM/DD/YYYY")}</h5>
+            <div id="day5Icon"><img src="http://openweathermap.org/img/w/${data.list[4].weather[0].icon}.png"/></div>
+            <div id="day5Temp">Temp: ${data.list[4].main.temp}&#xb0;F</div>
+            <div id="day5Hum">Humidity: ${data.list[4].main.humidity}%</div>
+        </div>
         `;
+
         $("#futureWeather").html(currentFive);
     })
     
-        
+    //logs err if what they inserted into the input box does not work
     }).catch(function(err) {
         console.log(err);
     })
@@ -186,6 +201,7 @@ function searchCity(word) {
 })
 }
 
+//runs searchCity function if a city in their history has been clicked
 $(".past-cities").on("click", "li", function(name) {
     searchCity(name.target.firstChild.data);
 })
